0b0467928d53d25dcdbac36d7cca838504446e80
Jacob Jensen <jaj.jacob.jensen@gmail.com>
2021-03-03 19:21:11 -0600

    feature/game

---
+++ script.js
@@ -0,0 +1,24 @@
+var character = document.querySelector(".character");
+var block = document.querySelector(".block");
+
+function jump() {
+    if (character.classList != "animate") {
+        character.classList.add("animate");
+    }
+    setTimeout(function() {
+        character.classList.remove("animate");
+    }, 500);
+}
+
+var checkDead = setInterval(function() {
+        var characterTop =
+            parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+        var blockLeft =
+            parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+        if (blockLeft < 20 && blockLeft > 0 && characterTop >= 130) {
+            block.style.animation = "none";
+            block.style.display = "none";
+            alert("You are not the greatest man that ever lived :(");
+        }
+    },
+    10);
